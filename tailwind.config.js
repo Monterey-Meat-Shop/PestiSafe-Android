0941c6a68a8d9710b33d2eb417155c013bcca406
Tani Sandoval <tanisandoval02@gmail.com>
2025-05-09 12:24:19 +0800

    Project Re-Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primary: '#006A67',
         accentLight: '#94E8B4',
         secondary: '#72BDA3',
         neutralDark: '#0C1B33',
         danger: '#FF6B6B',
-        light:{
+        light: {
           primary: '#2A8A86', // Lighter variant of primary
           accentLight: '#B3F0C9', // Even lighter variant of accentLight
           secondary: '#8CD6B9', // Lighter variant of secondary
           neutralDark: '#E0E5EB', // A light neutral, as a contrast to neutralDark
           danger: '#FF8C8C', // Lighter variant of danger
         },
-        dark:{
+        dark: {
           primary: '#004D4A', // Darker variant of primary
           accentLight: '#69B88A', // Darker, more saturated for dark background
           secondary: '#5AA78A', // Darker variant of secondary
@@ -29,4 +29,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
